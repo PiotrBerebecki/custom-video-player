38c6a53453bc9a35570d450a6880e0b6b009e389
PiotrBerebecki <petebere@gmail.com>
2017-01-31 21:18:04 +0000

    Wire up volume and speed change

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,4 @@
-//Get DOM Elements
+// Get DOM Elements
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -24,9 +24,26 @@ function skip(e) {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value; 
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  console.log(e);
+}
+
 // Event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+progress.addEventListener('click', scrub);
